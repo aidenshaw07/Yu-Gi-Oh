b482d54d4889b5338ca180793c1061129a1a82ed
frostycakes <lamquoc.24@gmail.com>
2023-01-17 17:04:53 -0500

    card search onSubmit rather than onChange

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,25 +2,27 @@ import "../styles/searchBarStyle.scss";
 import { useStore } from "../zustand/Store";
 
 const SearchBar = () => {
-  const state = useStore((cardName) => cardName);
+  
   const setFieldState = useStore((cardInfo) => cardInfo.setFieldState);
 
   return (
-    <div className="searchbar-container">
+    <form
+    //!Quoc was here
+      onSubmit={(event) => {
+        event.preventDefault();
+        setFieldState("searchedCardName", event.target[0].value);
+      }}
+      className="searchbar-container"
+    >
       <input
         type="text"
         className="searchbar-input-area"
-        value={state.searchedCardName}
         onFocus={(e) => (e.target.placeholder = "")}
-        onBlur={(e) =>
-          (e.target.placeholder = "Search For A Card")
-        }
-        onChange={(event) =>
-          setFieldState("searchedCardName", event.target.value)
-        }
+        onBlur={(e) => (e.target.placeholder = "Search For A Card")}
         placeholder="Search For A Card"
       />
-    </div>
+
+    </form>
   );
 };
 
